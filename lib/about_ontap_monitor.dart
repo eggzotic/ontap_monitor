a3afad96da1ac5911aa35218bd6bb1357948cdb1
Richard Shepherd <eggzotic@gmail.com>
2020-06-17 13:24:58 +1200

    Aligned README/About Content

--- lib/about_ontap_monitor.dart
+++ lib/about_ontap_monitor.dart
@@ -22,12 +22,20 @@ class AboutOntapMonitor extends StatelessWidget {
       body: ListView(
         children: [
           paragraph(
-              'Ontap Monitor is a sample app to demonstrate the use of the NetApp ONTAP REST API from a custom app.'),
+              'Ontap Monitor is a sample app to demonstrate the use of the NetApp ONTAP REST API from a custom app. That API is available from NetApp ONTAP 9.6 and later.'),
           paragraph(
-              'Written in Flutter - the UI toolkit from Google for creating apps for iOI, Android and the web from a single code-base - write-once, run multi-platform (almost WORM!). See https://flutter.dev'),
+              'For development purposes you can have your own running ONTAP Simulator - download the OVA from the NetApp support site at https://mysupport.netapp.com (NetApp SSO login required).'),
+          paragraph(
+              'Configuration storage is currently local-device-only (using SharedPreferences) for simplicity and to keep the repo self-contained and ready-to-run for new users. For a real enterprise app an external DB, with per-user authentication, and stronger encryption for credentials, would be required.'),
+          paragraph(
+              'The aim is to demonstrate how custom apps can be built that leverage the REST API for monitoring, configuration and provisioning of ONTAP resources. This could, for example, allow a Managed Services partner to create a self-branded, customer-needs-targeted - even self-service - application to monitor/manage their customers NetApp estate.'),
+          paragraph(
+              'This app can be run on iOS, Android and web using the appropriate "flutter run -d ..." device spec.'),
+          paragraph(
+              'Written in Flutter - the UI toolkit from Google for creating apps for iOS, Android and the web - all from a single code-base - write-once, run multi-platform (almost WORM!). See https://flutter.dev'),
           paragraph('''Richard Shepherd
 richard.shepherd3@netapp.com
-2020'''),
+June 2020'''),
         ],
       ),
     );
